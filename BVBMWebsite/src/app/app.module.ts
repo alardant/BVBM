7fba57596f0672d26712eb3dee4ad6942bb30f99
Antoine Lardant <antoine.lardant@gmail.com>
2023-09-19 12:43:49 +0200

    create home and button component

--- BVBMWebsite/src/app/app.module.ts
+++ BVBMWebsite/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { ButtonComponent } from './button/button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
+    HomeComponent,
+    ButtonComponent,
   ],
   imports: [
     BrowserModule,
